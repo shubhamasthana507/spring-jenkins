d476815cfdf5ec0de2b9d5de3b19dd8de5f8b62d
Shubham Asthana <shubhamasthana@Shubhams-MacBook-Air.local>
2023-10-26 14:47:17 +0530

    first commit

---
+++ src/test/java/com/jenkinsdemo/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.jenkinsdemo.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		assertThat(true);
+
+	}
+
+}

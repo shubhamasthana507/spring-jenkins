c6495db76638e4b844c7ffe2c006f458cd390d99
Shubham Asthana <shubhamasthana@Shubhams-MacBook-Air.local>
2023-10-26 17:24:21 +0530

    correct commit

--- src/test/java/com/jenkinsdemo/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkinsdemo/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertThat(true);
